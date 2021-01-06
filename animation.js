1b2a3da7e09cd926181ba68c2787a675595ba48c
poisonlylo <66483872+poisonlylo@users.noreply.github.com>
2021-01-06 18:14:02 +0100

    Update animation.js

--- animation.js
+++ animation.js
@@ -6,14 +6,15 @@ hamburger.addEventListener("click" , () => {
 })
 /*------------------- table-de-modules------------------*/
 //const mo = array.from(document.querySelectorAll(".table"))
-const table = document.querySelector(".table");
+
 const fstsemestre = document.querySelector(".fst-semestre ");
 
 /* fstsemestre.addEventListener("click" ,(e)=>{
     table.classList.add("fade-in");
 })
 */
-
+//bubble animation
+const table = document.querySelector(".table");
 const anim = document.querySelector(".anim");
 window.addEventListener("click" , () => {
     table.classList.add("fade");
